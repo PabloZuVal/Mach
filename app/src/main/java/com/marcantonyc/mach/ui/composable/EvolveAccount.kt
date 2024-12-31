4ce1e797ed721e403b4238329b9ba53972ac3275
Pablo ZuVal <pablo.zuniga.vale@gmail.com>
2024-12-30 22:36:28 -0300

    💄 Add image to component

--- app/src/main/java/com/marcantonyc/mach/ui/composable/EvolveAccount.kt
+++ app/src/main/java/com/marcantonyc/mach/ui/composable/EvolveAccount.kt
@@ -1,5 +1,6 @@
 package com.marcantonyc.mach.ui.composable
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -19,75 +20,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.marcantonyc.mach.R
 import com.marcantonyc.mach.ui.theme.MachTheme
 
 @Composable
 fun EvolveAccountCard(){
-    Card(
+    Image(
+        painter = painterResource(id = R.drawable.image1),
+        contentDescription = null,
         modifier = Modifier
             .fillMaxWidth()
-            .height(100.dp),
-        elevation = CardDefaults.elevatedCardElevation(
-            defaultElevation = 10.dp
-        ),
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.primary,
-        )
-    ){
-        Row(
-            modifier = Modifier
-                .weight(1f)
-                .padding(
-                    horizontal = 16.dp,
-                    vertical = 16.dp
-                )
-                .background(Color.Transparent)
-            ,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-
-
-            Column {
-                Text(
-                    text = "¡Evoluciona a \nCuenta Corriente!",
-                    color = MaterialTheme.colorScheme.onPrimary,
-                    style = TextStyle(
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight.Normal
-                    )
-                )
-                Spacer(modifier = Modifier.size(8.dp))
-
-                Text(
-                    text = "Sin costos adicionales",
-                    color = MaterialTheme.colorScheme.onPrimary,
-                    style = TextStyle(
-                        fontSize = 12.sp,
-                        fontWeight = FontWeight.Normal
-                    )
-                )
-
-            }
-            Column(
-                modifier = Modifier.weight(1f),
-                horizontalAlignment = Alignment.End
-            ){
-
-                Icon(
-                    imageVector = Icons.Default.ShoppingCart,
-                    contentDescription = "",
-                    modifier = Modifier.size(100.dp),
-                )
-            }
-
-        }
-    }
-
+            .height(130.dp)
+    )
 }
 
 @Preview
