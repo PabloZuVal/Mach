9cad403882311fe8e7f8aed1abaaa723ea1a4f86
Pablo ZuVal <pablo.zuniga.vale@gmail.com>
2025-01-04 19:10:52 -0300

    🗑️ Clean code

--- app/src/main/java/com/pablozv/mach/feature/inicio/ui/InicioScreen.kt
+++ app/src/main/java/com/pablozv/mach/feature/inicio/ui/InicioScreen.kt
@@ -12,44 +12,31 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.pablozv.mach.ui.theme.MachTheme
 import com.pablozv.mach.ui.composable.EvolveAccountCard
 import com.pablozv.mach.ui.composable.CardAccount
 import com.pablozv.mach.ui.composable.ScrollAccounts
-import com.pablozv.mach.ui.composable.SquareCard
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.pablozv.mach.core.navigation.BottonSheetController
 import com.pablozv.mach.ui.composable.HomeCardSection
 import com.pablozv.mach.ui.composable.ScrollImages
 import com.pablozv.mach.ui.composable.ShowMore
 
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable()
 fun InicioScreen(
     viewModel: InicioViewModel = hiltViewModel(),
     bottonSheetController: BottonSheetController
 ){
     val userData by viewModel.userData.collectAsState()
-
-    var showBottomSheet by remember { mutableStateOf(false) }
-    val sheetState = rememberModalBottomSheetState()
 
     Box(
         modifier = Modifier
